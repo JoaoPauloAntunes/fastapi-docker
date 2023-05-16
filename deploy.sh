623b2d33ec8633d25542d40f58db1de5a552f42d
João Paulo Antunes <48777730+JoaoPauloAntunes@users.noreply.github.com>
2023-05-16 10:29:43 -0300

    improv: Update script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Nome da imagem e contêiner
-IMAGE_NAME="api-image"
-CONTAINER_NAME="api-container"
-
 # Parar e remover o contêiner existente, se existir
-docker stop "$CONTAINER_NAME" >/dev/null 2>&1
-docker rm "$CONTAINER_NAME" >/dev/null 2>&1
+docker stop "fastapi-container" >/dev/null 2>&1
+docker rm "fastapi-container" >/dev/null 2>&1
 
 # Construir a nova imagem
-docker build -t "$IMAGE_NAME" .
+docker build -t "fastapi-image" .
 
 # Executar o novo contêiner
-docker run -d --name "$CONTAINER_NAME" -p 3302:80 "$IMAGE_NAME"
+docker run -d --name "fastapi-container" -p 3302:80 "fastapi-image"
 
 # Access this in browser: http://127.0.0.1:3302/items/5?q=somequery
